5f015207cf30b38806ff8e872a77a30ba0807d31
Adikauan <adi.kauan02@gmail.com>
2023-06-28 20:56:28 -0300

    clean code
    clean code

--- SKA.Calculator.Application/Commands/CalculateCommands/Calculate/CalculateCommandHandler.cs
+++ SKA.Calculator.Application/Commands/CalculateCommands/Calculate/CalculateCommandHandler.cs
@@ -8,16 +8,16 @@ namespace SKA.Calculator.Application.Commands.CalculateCommands.Calculate
     public class CalculateCommandHandler : IRequestHandler<CalculateCommand, string>
     {
         private readonly IMediator mediator;
+        private readonly CultureInfo culture;
 
         public CalculateCommandHandler(IMediator mediator)
         {
             this.mediator = mediator;
+            this.culture = CultureInfoHelper.SetCultureInfo();
         }
 
         public async Task<string> Handle(CalculateCommand request, CancellationToken cancellationToken)
         {
-            CultureInfo culture = CultureInfoHelper.SetCultureInfo();
-
             double result = CalculateHelper.CalculateValue(Double.Parse(request.Payload.FirstNumber, culture)
                 , double.Parse(request.Payload.SecondNumber, culture)
                 , request.Payload.Operation);
